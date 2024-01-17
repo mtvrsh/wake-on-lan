4998a89e73fab3847ccfbb71aea0169008c7d9ac
mtvrs <mtvrs@tuta.io>
2024-01-17 19:19:44 +0100

    Add simple testsuite

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+MAC="af:e2:d2:40:5c:16"
+BRD="192.168.0.255"
+
+after_1s() {
+	sleep 1s
+	"$@"
+}
+
+test_wol() {
+	if ./test_server.py $MAC $2 > /dev/null; then
+		echo "$1": OK
+	else
+		echo "$1": failed
+	fi
+}
+
+if command -v wol > /dev/null; then
+	after_1s wol $MAC > /dev/null &
+	test_wol "wol(1)"
+	after_1s wol $MAC -i $BRD > /dev/null &
+	test_wol "wol(1) subnet" "-i $BRD"
+fi
+
+after_1s python/wol.py $MAC &
+test_wol "python"
+after_1s python/wol.py -i $BRD $MAC &
+test_wol "python subnet" "-i $BRD"
+
+after_1s c/build/wol $MAC &
+test_wol "c"
+after_1s c/build/wol -i $BRD $MAC &
+test_wol "c subnet" "-i $BRD"
---
+++ test_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import argparse
+import socket
+import sys
+from python import wol
+
+# TODO timeout (5s)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", default="255.255.255.255")
+    parser.add_argument("-p", default=40000)
+    parser.add_argument("MAC")
+    args = parser.parse_args()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.bind((args.i, args.p))
+
+    resp, ip = s.recvfrom(150)
+    print(f"{len(resp)} bytes from {ip[0]}")
+
+    if len(resp) != 102:
+        print(f"bad packet length: {len(resp)}")
+        sys.exit(1)
+
+    if resp != wol.magic_from(args.MAC):
+        print("received magic packet does not match expected packet")
+        print(resp)
+        print([f"{b:#x}" for b in resp])
+        sys.exit(1)
