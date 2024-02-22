d91ea2f9d281400a723f34d99e529558b401ed2c
mtvrs <mtvrs@tuta.io>
2024-02-22 15:33:27 +0100

    python: do not create new socket object on every send
    Also make code more modular

--- python/wol.py
+++ python/wol.py
@@ -3,13 +3,10 @@ import re
 import socket
 
 
-def send_magic(mac_address: str, addr: tuple[str, int]):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.sendto(magic_from(mac_address), addr)
+def send_magic(mac_address: str, addr="255.255.255.255", port=40000):
+    WakeOnLan(addr, port).send_magic(mac_address)
 
 
-# for testing
 def magic_from(mac_address: str) -> bytes:
     m = bytes.fromhex(mac_address.replace(":", "").replace("-", ""))
     return b"\xff" * 6 + m * 16
@@ -21,6 +18,18 @@ def is_valid_mac(mac: str) -> bool:
             r"[0-9a-f]{2}([:-]?)[0-9a-f]{2}(\1[0-9a-f]{2}){4}$", mac.lower()
         )
     )
+
+
+class WakeOnLan:
+    def __init__(self, addr: str, port: int):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self._socket.connect((addr, port))
+
+    def send_magic(self, mac_address: str):
+        n = self._socket.send(magic_from(mac_address))
+        if n != 102:
+            raise RuntimeError("sent only part of magic packet")
 
 
 if __name__ == "__main__":
@@ -45,8 +54,10 @@ if __name__ == "__main__":
     parser.add_argument("MAC", nargs="+", help="address of device to wake")
     args = parser.parse_args()
 
+    wol = WakeOnLan(args.i, args.p)
+
     for m in args.MAC:
         if not is_valid_mac(m):
             print(f"invalid mac address: {m}", file=sys.stderr)
             continue
-        send_magic(m, (args.i, args.p))
+        wol.send_magic(m)
